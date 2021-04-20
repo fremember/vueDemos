67bef3396a2901293ad9d0ada1320bab2491ad16
fremember <18769567910@163.com>
2021-04-20 10:57:16 +0800

    删除代码

--- vue-admin/src/api/router.js
+++
@@ -1,8 +0,0 @@
-import request from "@/utils/request"
-let api = '/api'
-/**
- * 根据用户标识获取其权限对应的路由
- */
-export function getRouterList (params) {
-    return request.get(`${api}/common/userRouter`, params)
-}
--- zhaopin/src/service/model/chat/index.js
+++
@@ -1,8 +0,0 @@
-module.exports.chat =  {
-	'chatid': {  'type': String, 'require': true },// 信息的唯一标识
-	'from': { 'type': String, 'require': true },// 信息来自谁
-	'to': { 'type': String, 'require': true },// 信息发送给谁
-	'read': { 'type': Boolean, 'default': false },// 消息已读或未读的状态，且该字段只对to所对应的人有作用
-	'content': { 'type': String, 'require': true, 'default': '' },// 信息内容
-	'timeStamp': { 'type': Number, 'default': 0 }// 信息创建时间
-};
