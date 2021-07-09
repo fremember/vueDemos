468c5fb5cc773e1ff172140cdf127da9cb16d095
fremember <605941383@qq.com>
2021-07-09 11:28:47 +0800

    新增props组件

---
+++ vue3/src/views/props/index.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: fremember
+ * @Date: 2021-07-09 11:25:34
+ * @Description: 
+-->
+<template>
+    <div>
+    </div>
+</template>
+<script type="text/ecmascript-6">
+    export default {
+        name: 'VerificationCode',
+        setup () {
+            return {}
+        }
+    }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
